9c25c10aaa26c09d36e6797d7f0829cb6cd875a8
Eli Joe <elijoe365@pm.me>
2023-04-28 05:49:45 +0100

    Do one at a time on HPC

--- script/train_simplenet.sh
+++ script/train_simplenet.sh
@@ -1,8 +1,8 @@
-python ../cifar10/main.py --batch_size 128 --optimizer sgd --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 8192 --optimizer sgd --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 128 --optimizer sgd --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 8192 --optimizer sgd --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 128 --optimizer adam --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 8192 --optimizer adam --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 128 --optimizer adam --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet &
-python ../cifar10/main.py --batch_size 8192 --optimizer adam --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+python ../cifar10/main.py --batch_size 128 --optimizer sgd --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 8192 --optimizer sgd --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 128 --optimizer sgd --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 8192 --optimizer sgd --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 128 --optimizer adam --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 8192 --optimizer adam --weight_decay 0 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 128 --optimizer adam --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
+#python ../cifar10/main.py --batch_size 8192 --optimizer adam --weight_decay 0.0005 --epochs 100 --save_epoch 1 --rand_seed 42 --model simplenet
